a77c032d8fc9a0deef09eccb5d9ac15dcad33d87
Leire Gonzalez <lhocasar@gmail.com>
2015-07-04 18:07:27 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -47,3 +47,19 @@ exports.index = function (req,res){
 exports.author = function(req,res){
 	res.render('author', {autor: 'Leire González Hocasar'});
 };
+
+//GET /quizes/new
+exports.new = function(req,res){
+  var quiz = models.Quiz.build(
+	{pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+  res.render('quizes/new', {quiz: quiz});
+};
+
+//POST /quizes/create
+exports.create = function(req, res){
+  var quiz = models.Quiz.build(req.body.quiz);
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');
+  })
+};
