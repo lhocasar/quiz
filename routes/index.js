034401b86a47ce12cc70d8a540f970a47eef52fe
Leire Gonzalez <lhocasar@gmail.com>
2015-07-08 00:52:41 +0200

    Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,7 +13,12 @@ router.get('/', function(req, res) {
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
 
-//Deficion de rutas de /quizes
+//Definición de rutas de sesion
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
+
+//Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
